ff898b79a62d805c75db3dff79f966430be6219f
AbbasGori <abbasg@decisionminds.com>
2018-11-14 13:29:15 +0530

    run

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
 
-    return "Hello World!"
+    return "Hello World!run"
 
 if __name__ == '__main__':
     app.run(host='ec2-54-197-10-139.compute-1.amazonaws.com',port=80)
